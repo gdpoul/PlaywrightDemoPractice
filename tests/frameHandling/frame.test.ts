11e2a78fb09270c47f881b8dec7e644919eff54d
Ganesh_Poul <gdpoul333@gmail.com>
2023-03-01 12:41:34 +0530

    added frame code

---
+++ tests/frameHandling/frame.test.ts
@@ -0,0 +1,23 @@
+import {expect, test} from "@playwright/test";
+
+test("Frame Handle", async({page})=>{
+    await page.goto("https://letcode.in/frame");
+
+    const frame= page.frame({name:"firstFr"});
+    await frame?.fill("input[name='fname']","Ganesh");
+    await frame?.fill("input[name='lname']","Poul");
+
+    const innerframes= frame?.childFrames();
+    console.log("No of inner frame :"+innerframes?.length);
+    if(innerframes != null){
+        await innerframes[1].fill("input[name='email']","gdpoul@gmail.com"); 
+    }
+
+  
+
+    // if(frame !=null){
+    //     await frame?.fill("input[name='fname']","Ganesh");
+    //     await frame?.fill("input[name='lname']","Poul");
+    // }
+    // else throw new Error("No Such Frame"); 
+})
